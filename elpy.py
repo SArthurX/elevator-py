b2208cd9f4010383c57d9f35c72ec78f959106a4
SArthurX <sxa05z@gmail.com>
2023-06-09 17:17:47 +0800

    add req.txt

--- elpy.py
+++ elpy.py
@@ -5,8 +5,8 @@ from floor import  hand_pos
 import time
 from pyfirmata2 import Arduino
 
-board = Arduino('COM5')
-#board = Arduino(Arduino.AUTODETECT)
+#board = Arduino('COM5')
+board = Arduino(Arduino.AUTODETECT)
 pin =[5,6,7,8,9,10,11,12,13]
 
 nled = {1,2,3,4,5,6,7,8,9}
